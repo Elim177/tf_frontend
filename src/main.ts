16d9d4d45ff37140338cd756f525e44a50ba5353
Hykilpikonna <me@hydev.org>
2022-06-21 17:04:41 -0400

    [+] Add VueYoutubeEmbed dependency

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import router from "./router";
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import VueYouTubeEmbed from 'vue-youtube-embed'
+
 export const $ = (window as any).jQuery
 
-createApp(App).use(router).use(ElementPlus).mount('#app')
+createApp(App).use(router).use(ElementPlus).use(VueYouTubeEmbed).mount('#app')
