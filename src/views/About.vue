bed11645fe7fcfdc602a7b4f4343769c26b52b4c
Suha Siddiqui <suhasid@my.yorku.ca>
2022-08-18 00:10:17 -0400

    image 2

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,9 +3,9 @@
         <hr>
         <p>Under the supervision of Professor Maleknaz Nayebi and the Faculty of Lassonde School of Engineering in the Department of EECS, worked on a project titled "Customizing and Tailoring Machine Learning Documentation". Duties and Responsibilities consist of gathering data, pre-processing and mining the metadata, pre-processing and mining images, auto-captioning, and operating databases. The research project is presented at the Lassonde Undergraduate Research Conference.</p>
     </div>
-    <img src = "@/assets/Slide1.png">
+    <!-- <img src = "@/assets/Slide1.png"> -->
     <img src = "@/assets/Slide2.png">
-    <img src = "@/assets/Slide3.png">
+    <!-- <img src = "@/assets/Slide3.png"> -->
     <!-- <img src = "@/assets/Slide4.png"> -->
     <!-- <img src = "@/assets/Slide5.png">
     <img src = "@/assets/Slide6.png">
@@ -25,9 +25,9 @@
 
 <script lang="ts">
 import {Options, Vue} from 'vue-class-component';
-import Slide1 from '@/assets/Slide1.png';
+// import Slide1 from '@/assets/Slide1.png';
 import Slide2 from '@/assets/Slide2.png';
-import Slide3 from '@/assets/Slide3.png';
+// import Slide3 from '@/assets/Slide3.png';
 // import Slide4 from '@/assets/Slide4.png';
 // import Slide5 from '@/assets/Slide5.png';
 // import Slide6 from '@/assets/Slide6.png';
@@ -42,9 +42,9 @@ import Slide3 from '@/assets/Slide3.png';
 // import Slide15 from '@/assets/Slide15.png';
 // import Slide16 from '@/assets/Slide16.png';
 // import Slide17 from '@/assets/Slide17.png';
-console.log(Slide1);
+// console.log(Slide1);
 console.log(Slide2);
-console.log(Slide3);
+// console.log(Slide3);
 // console.log(Slide4);
 // console.log(Slide5);
 // console.log(Slide6);
