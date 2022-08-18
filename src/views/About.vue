58a6d5f00aecb5c3049965b43b833d0e845167b0
Suha Siddiqui <suhasid@my.yorku.ca>
2022-08-17 22:53:42 -0400

    test

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,7 +7,7 @@
     <img src = "@/assets/Slide2.png">
     <img src = "@/assets/Slide3.png">
     <img src = "@/assets/Slide4.png">
-    <img src = "@/assets/Slide5.png">
+    <!-- <img src = "@/assets/Slide5.png">
     <img src = "@/assets/Slide6.png">
     <img src = "@/assets/Slide7.png">
     <img src = "@/assets/Slide8.png">
@@ -19,7 +19,7 @@
     <img src = "@/assets/Slide14.png">
     <img src = "@/assets/Slide15.png">
     <img src = "@/assets/Slide16.png">
-    <img src = "@/assets/Slide17.png">
+    <img src = "@/assets/Slide17.png"> -->
     
 </template>
 
@@ -29,36 +29,36 @@ import Slide1 from '@/assets/Slide1.png';
 import Slide2 from '@/assets/Slide2.png';
 import Slide3 from '@/assets/Slide3.png';
 import Slide4 from '@/assets/Slide4.png';
-import Slide5 from '@/assets/Slide5.png';
-import Slide6 from '@/assets/Slide6.png';
-import Slide7 from '@/assets/Slide7.png';
-import Slide8 from '@/assets/Slide8.png';
-import Slide9 from '@/assets/Slide9.png';
-import Slide10 from '@/assets/Slide10.png';
-import Slide11 from '@/assets/Slide11.png';
-import Slide12 from '@/assets/Slide12.png';
-import Slide13 from '@/assets/Slide13.png';
-import Slide14 from '@/assets/Slide14.png';
-import Slide15 from '@/assets/Slide15.png';
-import Slide16 from '@/assets/Slide16.png';
-import Slide17 from '@/assets/Slide17.png';
+// import Slide5 from '@/assets/Slide5.png';
+// import Slide6 from '@/assets/Slide6.png';
+// import Slide7 from '@/assets/Slide7.png';
+// import Slide8 from '@/assets/Slide8.png';
+// import Slide9 from '@/assets/Slide9.png';
+// import Slide10 from '@/assets/Slide10.png';
+// import Slide11 from '@/assets/Slide11.png';
+// import Slide12 from '@/assets/Slide12.png';
+// import Slide13 from '@/assets/Slide13.png';
+// import Slide14 from '@/assets/Slide14.png';
+// import Slide15 from '@/assets/Slide15.png';
+// import Slide16 from '@/assets/Slide16.png';
+// import Slide17 from '@/assets/Slide17.png';
 console.log(Slide1);
 console.log(Slide2);
 console.log(Slide3);
 console.log(Slide4);
-console.log(Slide5);
-console.log(Slide6);
-console.log(Slide7);
-console.log(Slide8);
-console.log(Slide9);
-console.log(Slide10);
-console.log(Slide11);
-console.log(Slide12);
-console.log(Slide13);
-console.log(Slide14);
-console.log(Slide15);
-console.log(Slide16);
-console.log(Slide17);
+// console.log(Slide5);
+// console.log(Slide6);
+// console.log(Slide7);
+// console.log(Slide8);
+// console.log(Slide9);
+// console.log(Slide10);
+// console.log(Slide11);
+// console.log(Slide12);
+// console.log(Slide13);
+// console.log(Slide14);
+// console.log(Slide15);
+// console.log(Slide16);
+// console.log(Slide17);
 @Options({components: {}})
 export default class About extends Vue
 {
