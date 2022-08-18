4c96847490b4a460e3268ada2c95ebe4e1c66222
Suha Siddiqui <suhasid@my.yorku.ca>
2022-08-17 20:56:32 -0400

    replaced jpg to png

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,45 +2,9 @@
     <div id="About">
         <hr>
         <p>Under the supervision of Professor Maleknaz Nayebi and the Faculty of Lassonde School of Engineering in the Department of EECS, worked on a project titled "Customizing and Tailoring Machine Learning Documentation". Duties and Responsibilities consist of gathering data, pre-processing and mining the metadata, pre-processing and mining images, auto-captioning, and operating databases. The research project is presented at the Lassonde Undergraduate Research Conference.</p>
-        <hr>
-        
-        <img src="src\assets\Slide1.jpg" style="width:100%">
         <br>
-        <img src="src\assets\Slide2.jpg" style="width:100%">
         <br>
-        <img src="src\assets\Slide3.jpg" style="width:100%">
-        <br>
-        <img src="src\assets\Slide4.jpg" style="width:100%">
-        <br>
-        <img src="src\assets\Slide5.jpg" style="width:100%">
-        <br>
-        <img src="src\assets\Slide6.jpg" style="width:100%">
-        <br>
-        <img src="src\assets\Slide7.jpg" style="width:100%">
-        <br>
-        <img src="src\assets\Slide8.jpg" style="width:100%">
-        <br>
-        <img src="src\assets\Slide9.jpg" style="width:100%">
-        <br>
-        <img src="src\assets\Slide10.jpg" style="width:100%">
-        <br>
-        <img src="src\assets\Slide11.jpg" style="width:100%">
-        <br>
-        <img src="src\assets\Slide12.jpg" style="width:100%">
-        <br>
-        <img src="src\assets\Slide13.jpg" style="width:100%">
-        <br>
-        <img src="src\assets\Slide14.jpg" style="width:100%">
-        <br>
-        <img src="src\assets\Slide15.jpg" style="width:100%">
-        <br>
-        <img src="src\assets\Slide16.jpg" style="width:100%">
-        <br>
-        <img src="src\assets\Slide17.jpg" style="width:100%">
-        <br>
-
-            
-            
+        <img src="@/assets/slide1.png" alt="">
     </div>
 </template>
 
@@ -60,7 +24,4 @@ export default class About extends Vue
         font-size: 0.8em
 </style>
 
-<style>
 
-
-</style>
