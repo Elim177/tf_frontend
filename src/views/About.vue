9be1219f1773e9fe2dad64664c774ac3b4834974
Suha Siddiqui <suhasid@my.yorku.ca>
2022-08-17 20:01:18 -0400

    diff path to images

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,87 +8,87 @@
             <!-- Full-width images with number and caption text -->
             <div class="mySlides fade">
                 <div class="numbertext">1 / 17</div>
-                <img src="/src/assets/Slide1.jpg" style="width:100%">
+                <img src="src\assets\Slide1.jpg" style="width:100%">
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">2 / 17</div>
-                <img src="/src/assets/Slide2.jpg" style="width:100%">
+                <img src="src\assets\Slide2.jpg" style="width:100%">
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">3 / 17</div>
-                <img src="/src/assets/Slide3.jpg" style="width:100%">
+                <img src="src\assets\Slide3.jpg" style="width:100%">
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">4 / 17</div>
-                <img src="/src/assets/Slide4.jpg" style="width:100%">
+                <img src="src\assets\Slide4.jpg" style="width:100%">
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">5 / 17</div>
-                <img src="/src/assets/Slide5.jpg" style="width:100%">
+                <img src="src\assets\Slide5.jpg" style="width:100%">
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">6 / 17</div>
-                <img src="/src/assets/Slide6.jpg" style="width:100%">
+                <img src="src\assets\Slide6.jpg" style="width:100%">
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">7 / 17</div>
-                <img src="/src/assets/Slide7.jpg" style="width:100%">
+                <img src="src\assets\Slide7.jpg" style="width:100%">
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">8 / 17</div>
-                <img src="/src/assets/Slide8.jpg" style="width:100%">
+                <img src="src\assets\Slide8.jpg" style="width:100%">
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">9 / 17</div>
-                <img src="/src/assets/Slide9.jpg" style="width:100%">
+                <img src="src\assets\Slide9.jpg" style="width:100%">
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">10 / 17</div>
-                <img src="/src/assets/Slide10.jpg" style="width:100%">
+                <img src="src\assets\Slide10.jpg" style="width:100%">
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">11 / 17</div>
-                <img src="/src/assets/Slide11.jpg" style="width:100%">
+                <img src="src\assets\Slide11.jpg" style="width:100%">
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">12 / 17</div>
-                <img src="/src/assets/Slide12.jpg" style="width:100%">
+                <img src="src\assets\Slide12.jpg" style="width:100%">
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">13 / 17</div>
-                <img src="/src/assets/Slide13.jpg" style="width:100%">
+                <img src="src\assets\Slide13.jpg" style="width:100%">
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">14 / 17</div>
-                <img src="/src/assets/Slide14.jpg" style="width:100%">
+                <img src="src\assets\Slide14.jpg" style="width:100%">
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">15 / 17</div>
-                <img src="/src/assets/Slide15.jpg" style="width:100%">
+                <img src="src\assets\Slide15.jpg" style="width:100%">
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">16 / 17</div>
-                <img src="/src/assets/Slide16.jpg" style="width:100%">
+                <img src="src\assets\Slide16.jpg" style="width:100%">
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">17 / 17</div>
-                <img src="/src/assets/Slide17.jpg" style="width:100%">
+                <img src="src\assets\Slide17.jpg" style="width:100%">
             </div>
 
             </div>
